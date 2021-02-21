e8f7aaaf733b077eaedc00055ffc1e7f9f0c8380
Lord-Giganticus <mrubins64@gmail.com>
2021-02-21 15:08:04 -0600

    add more cmd scripts

--- build.cmd
+++ build.cmd
@@ -1,2 +1,4 @@
 msbuild Spotlight\Spotlight.sln -p:Configuration="Release Spotlight"
 msbuild Spotlight\Spotlight.sln -p:Configuration="Release Moonlight"
+msbuild Spotlight\Spotlight.sln -p:Configuration="Debug Spotlight"
+msbuild Spotlight\Spotlight.sln -p:Configuration="Debug Moonlight"
---
+++ zip.cmd
@@ -0,0 +1,12 @@
+cd Spotlight\SpotLight\bin\Release
+7z a -r "Spotlight Release.zip" *
+mv "Spotlight Release.zip" ../../../../.
+cd "../Release OdysseySupport"
+7z a -r "Moonlight Release.zip" *
+mv "Moonlight Release.zip" ../../../../.
+cd ../Debug
+7z a -r "Spotlight Debug.zip" *
+mv "Spotlight Debug.zip" ../../../../.
+cd "../Debug OdysseySupport"
+7z a -r "Moonlight Debug.zip" *
+mv "Moonlight Debug.zip" ../../../../.
