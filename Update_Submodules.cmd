ec370056cf7632ed6abed20e31f1fcd0e53b9b9c
Lord-Giganticus <mrubins64@gmail.com>
2021-03-02 21:41:25 -0600

    Spotlight got updated finally lol

--- Update_Submodules.cmd
+++ Update_Submodules.cmd
@@ -1,2 +1,7 @@
 git submodule init
 git submodule update
+cd GL_editorFramework
+git pull https://github.com/jupahe64/GL_EditorFramework.git master
+cd ..\Spotlight
+git pull https://github.com/jupahe64/Spotlight.git master
+cd ..\
